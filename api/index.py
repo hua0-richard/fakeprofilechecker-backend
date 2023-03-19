2e90fa88e451b44d7f5847ec0292a23746b10821
Richard Hua <richardhua@Richards-MacBook-Air.local>
2023-03-19 16:09:08 -0400

    fixed api key

--- api/index.py
+++ api/index.py
@@ -10,7 +10,7 @@ import snscrape.modules.twitter as sntwitter
 import openai
 
 # openAI key
-openai.api_key = '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' 
+openai.api_key = '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' 
 
 # timeout imports
 import signal
